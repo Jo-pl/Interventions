09953fb4e7773c6c0b4784b5c6f82dbc6efb9e16
Joel Pimpare <plarocque.joel@gmail.com>
2019-03-13 17:36:36 -0400

    Partie6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccueilComponent} from './accueil/accueil.component';
+import {ProblemeComponent} from './probleme/probleme.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'accueil',component:AccueilComponent},
+  {path:'', redirectTo:'accueil',pathMatch:'full'},
+  {path:'**', redirectTo:'Accueil',pathMatch:'full'}, //if the route doesn't exist redirects automatically the user to "Accueil"
+  {path:'probleme',component:ProblemeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import{RouterModule} from '@angular/router';
+import { ProduitComponent } from './produit/produit.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent,
+    ProduitComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
